51f23936e50a7fbbe01d9c41ff73600a45e768d3
Sergey Mironov <grrwlf@gmail.com>
2013-07-25 18:52:40 +0400

    Rewrite Writer in monadic style

--- src/Development/Cake3/Writer.hs
+++ src/Development/Cake3/Writer.hs
@@ -6,35 +6,34 @@ import Data.List
 import Development.Cake3.Types
 import Text.Printf
 
-toMake tree = unlines $ body where
+rule2vars r = let Uniq vs = fst $ collapse' (rvars r) in vs
 
-  body = guard ++ (map varToken vs) ++ (concat $ map ruleToken rs) ++ (concat $ map guardRuleOf vs)
+tell1 x = tell [x]
 
-  (Uniq vs, Uniq rs, es) = collapse tree
+toMake tree = let (Uniq vs, Uniq rs, es) = collapse tree in unlines $ execWriter $ do
 
-  varToken (Variable n v) = printf "%s = %s" n v
+  tell1 "GUARD = $(1)_GUARD_$(shell echo $($(1)) | md5sum | cut -d ' ' -f 1)"
 
-  guard = ["GUARD = $(1)_GUARD_$(shell echo $($(1)) | md5sum | cut -d ' ' -f 1)"]
+  forM vs $ \(Variable n v) -> do
+    tell1 $ printf "%s = %s" n v
 
-  guardRuleOf v = [
-      printf "$(call GUARD,%s) :" (vname v)
-    , printf "\trm -f %s_GUARD_*" (vname v)
-    , printf "\ttouch $@"
-    , ""
-    ]
-
-  rule2vars r = let Uniq vs = fst $ collapse' (rvars r) in vs
-
-  -- FIXME: implement http://stackoverflow.com/questions/11647859/make-targets-depend-on-variables
-  -- FIXME: extra newline in commands
-  ruleToken r = execWriter $ do
+  forM rs $ \r -> do
+    let varguard v = printf "$(call GUARD,%s)" (vname v)
+    let deps = (intercalate " " $ (rsrc r) ++ (map varguard (rule2vars r)))
     when (rphony r) $ do
-      tell [printf ".PHONY: %s" (rtgt r)]
-    tell $ hdr : cmds
-    where
-      hdr = printf "%s : %s # %s" (rtgt r) deps (rloc r)
-      cmds = map ("\t" ++) (rcmd r)
-      varguard v = printf "$(call GUARD,%s)" (vname v)
-      deps = (intercalate " " $ (rsrc r) ++ (map varguard (rule2vars r)))
+      tell1 $ printf ".PHONY: %s" (rtgt r)
+    tell1 $ printf "%s : %s # %s" (rtgt r) deps (rloc r)
+    forM (rcmd r) $ \c -> tell1 ("\t" ++ c)
 
-
+  forM vs $ \v -> do
+    tell $ [
+        printf "$(call GUARD,%s) :" (vname v)
+      , printf "\trm -f %s_GUARD_*" (vname v)
+      , printf "\ttouch $@"
+      -- FIXME: why do we need this empty line?
+      , ""
+      ]
+  
+  
+  
+  
