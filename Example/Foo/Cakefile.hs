1016cb7069cf4a065a02d09f1479bfb3629dc1e7
Sergey Mironov <grrwlf@gmail.com>
2013-07-26 20:34:17 +0400

    Foo example: adjust Cakefile

--- Example/Foo/Cakefile.hs
+++ Example/Foo/Cakefile.hs
@@ -9,7 +9,11 @@ import Development.Cake3
 import Cakepath_Main (file)
 import qualified CakeLib as L
 
-cflags = var "CFLAGS" "-O0 -g3"
+sound = "Yuupee" :: String
+
+cflags = makevar "CFLAGS" "-O0 -g3"
+
+shell = extvar "SHELL"
 
 cfiles = map file [ "main.c"]
 
@@ -18,10 +22,22 @@ ofiles = do
     rule [c .= "o"] $ do
       [make| gcc -I lib -c $cflags -o $dst $c |]
 
+allofiles = ofiles ++ (L.ofiles cflags)
+
 elf = do
   rule [file "main.elf"] $ do
-    [make| gcc -o $dst $ofiles $(L.ofiles cflags) |]
+    [make| echo "SHELL is $shell" |]
+    [make| gcc -o $dst $allofiles |]
+    [make| echo $sound |]
+
+clean = do
+  phony "clean" $ do
+    unsafe $ do
+      [make| rm $elf ; rm GUARD_* ; rm $allofiles |]
+
+all = do
+  phony "all" $ depend (head elf)
 
 main = do
-  runMake elf >>= putStrLn . toMake
+  runMake (Main.all ++ elf ++ clean) >>= putStrLn . toMake
 
--- Example/Foo/build.sh
+++ Example/Foo/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+set +x
+
 ghc -ilib --make Cakefile.hs && ./Cakefile | tee Makefile
+
+echo "Makefile created. Type make to compile"
 
