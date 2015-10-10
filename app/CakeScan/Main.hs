a6e2b337ccfdbfddd6ea15ba06dd5031879bcdf3
Sergey Mironov <grrwlf@gmail.com>
2015-10-10 07:18:27 +0000

    cake3-scan implement simple search

--- app/CakeScan/Main.hs
+++ app/CakeScan/Main.hs
@@ -1,20 +1,30 @@
 module Main where
 
 import Control.Monad
+import Data.List
 
 import System.Environment
 import System.Process
 import System.Exit
 
-import System.FilePath.Wrapper
+import System.FilePath
+import System.FilePath.Wrapper (FileT(..))
 import Development.Cake3.Utils.Find
 
+filterCakes :: FilePath -> Bool
+filterCakes x = nodots && isPrefixOf "Cake" bn &&  isSuffixOf ".hs" bn where
+  bn = takeFileName x
+  nodots = all (not . isPrefixOf ".") $ skipRelativeDot $ splitDirectories x
+  skipRelativeDot (".":ds) = ds
+  skipRelativeDot x = x
+
+toFilePath (FileT () x) = x
 
 main :: IO ()
 main = do
-  fs <- getDirectoryContentsRecursive (FileT () ".")
+  fs <- filter filterCakes <$> map toFilePath <$> getDirectoryContentsRecursive (FileT () ".")
 
-  forM_ fs $ \(FileT () x) -> do
+  forM_ fs $ \x -> do
     putStrLn x
 
   return ()
